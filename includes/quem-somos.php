3283683d4b3246823c2d9a43c8f32c44362a8b99
Aline-Cardoso <aline.cardosogomes@gmail.com>
2022-02-02 20:55:27 -0300

    Commit Inicial

---
+++ includes/quem-somos.php
@@ -0,0 +1,23 @@
+<section style="background: #008191;">
+    <div class="container pt-md-4 pt-xl-0">
+        <div class="row align-items-center justify-content-center pb-5">
+            <div class="col-lg-6 pb-sm-4 pb-lg-0 mb-5 mb-lg-0 pe-3">
+                <div class="overflow-hidden">
+                    <h2 class="font-weight-bold line-height-1 mb-0 appear- text-light" data-appear-animation="maskUp" data-appear-animation-delay="300">Sobre a LEDAX</h2>
+                </div>
+                <div class="custom-divider divider divider-primary divider-small my-3">
+                    <hr class="my-0 appear-animation" data-appear-animation="customLineProgressAnim" data-appear-animation-delay="700">
+                </div>
+                <p class="pb-1 mb-4 appear-animation text-light text-justify" data-appear-animation="fadeInUpShorter" data-appear-animation-delay="450">Olá, nós somos a LEDAX Energia Inteligente, empresa com expertise em fornecer soluções de eficiência energética através de iluminação LED, sistemas de geração fotovoltaica, monitoramento e gestão de consumo de energia por sistema proprietário, eletromobilidade – carregadores para veículos elétricos – e serviços de migração e gestão de contratos para o Mercado Livre de Energia.</p>
+                <p class="pb-1 mb-4 appear-animation text-light text-justify" data-appear-animation="fadeInUpShorter" data-appear-animation-delay="700">Atuamos no mercado B2B e as principais áreas de atuação dos nossos serviços são: indústrias, supermercados, centros de distribuição, hospitais, shoppings centers e iluminação pública.</p>
+                <p class="pb-1 mb-4 appear-animation text-light text-justify" data-appear-animation="fadeInUpShorter" data-appear-animation-delay="950">A unidade industrial para fabricação das nossas luminárias LED, que conta com equipamentos modernos e de tecnologia avançada, fica localizada em Salvador/BA. Possuímos também escritórios em Salvador/BA, Belo Horizonte/MG e Porto Alegre/RS.</p>
+                <div class="d-flex align-items-start align-items-sm-center flex-column flex-sm-row">
+                    <a href="<?=$urlcompleta;?>institucional" class="btn btn-light custom-btn-border-radius font-weight-bold text-3 px-5 btn-py-3 me-sm-2 mb-3 mb-sm-0 appear-animation" data-appear-animation="fadeInRightShorterPlus" data-appear-animation-delay="950">SAIBA MAIS</a>
+                </div>
+            </div>
+            <div class="col-lg-6 pb-sm-4 pb-lg-0 mb-5 mb-lg-0">
+             <iframe src="https://player.vimeo.com/video/358887504?h=66311b2f44" width="100%" height="360" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
+         </div>
+     </div>
+ </div>
+</section>
---
+++ includes/top-bar.php
@@ -0,0 +1,31 @@
+<div class="header-column justify-content-start">
+    <div class="header-row">
+        <nav class="header-nav-top">
+            <ul class="nav nav-pills">
+                <li class="nav-item nav-item-borders py-2">
+                    <a target="_blank" href="<?= $whatslink; ?>"><i class="fab fa-whatsapp text-4" style="top: 0; color: #008191;"></i><?= $ddd; ?> <?= $whats; ?></a>
+                </li>
+                <li class="nav-item nav-item-borders py-2 d-none d-md-inline-flex">
+                    <a target="_blank" href="<?= $tellink; ?>"><i class="fa fa-phone text-4" style="top: 0px; color: #008191;"></i><?= $ddd; ?> <?= $tel; ?></a>
+                </li>
+                <li class="nav-item nav-item-borders py-2 d-none d-md-inline-flex">
+                    <a target="_blank" href="mailto:<?=$email;?>"><i class="far fa-envelope text-4" style="top: 0px; color: #008191;"></i><?= $email; ?></a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</div>
+<div class="header-column justify-content-end">
+    <div class="header-row">
+        <nav class="header-nav-top">
+            <ul class="nav nav-pills">
+                <li class="nav-item nav-item-borders py-2 d-none d-lg-inline-flex">
+                    <ul class="header-social-icons social-icons social-icons-clean">
+                        <li class="social-icons-facebook"><a href="<?= $linkFace; ?>" target="_blank" title="Facebook"><i class="fab fa-facebook-f"></i></a></li>
+                        <li class="social-icons-instagram"><a href="<?= $linkInstagram; ?>" target="_blank" title="Instagram"><i class="fab fa-instagram"></i></a></li>
+                        <li class="social-icons-linkedin"><a href="<?= $linkedIn; ?>" target="_blank" title="Linkedin"><i class="fab fa-linkedin"></i></a></li>
+                    </ul>
+            </ul>
+        </nav>
+    </div>
+</div>
